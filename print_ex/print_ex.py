0140a9989f0b701498f109df0fe486b83500d968
tohru <tohru246ra.63099@gmail.com>
2022-12-07 14:23:12 +0900

    append  print_ex

---
+++ print_ex/print_ex.py
@@ -0,0 +1,19 @@
+# print(*objects, sep='', end='\n', file=sys.stdout, flush=False)
+
+mugiwara = ["Luffy","Zoro","Nami"]
+
+print("Hello")                      # Hello
+
+print(mugiwara)                     # ['Luffy', 'Zoro', 'Nami']
+print(*mugiwara)                    # Luffy Zoro Nami
+
+print(*mugiwara, sep=' $ ')         # Luffy $ Zoro $ Nami
+print(*mugiwara, end=' $ ')         # Luffy Zoro Nami $   <- 改行していない
+print("")                           # 改行
+     
+for i in mugiwara:                  # Luffy
+    print(i)                        # Zoro
+                                    # Nami  <- 列で出力
+for i in mugiwara:
+    print(i, end=' ')               #Luffy Zoro Nami  <- 行で出力
+
