46c76d083f3d03f26c2ac423c070a9ea39331e66
Tohru-246ra <108559601+Tohru-246ra@users.noreply.github.com>
2023-02-06 09:43:56 +0900

    Update characterCode.py

--- others/characterCode.py
+++ others/characterCode.py
@@ -1,6 +1,6 @@
-alp=[chr(ord('A')+i) for i in range(26)]
-# print(ord('あ')) #ord 文字→コードポイント(10進数,Unicode)
-# print(hex(ord('あ')))
-# print(chr(0x3042)) #chr: コードポイント→文字
-hiragana=[chr(ord('あ')+i) for i in range(50)]
-print(hiragana)
+ord('あ')    #12354 #ord: 文字→コードポイント(10進数,Unicode)
+
+chr(12354)   #あ    #chr: コードポイント→文字
+
+Alp=[chr(ord('A')+i) for i in range(26)] # 大文字のアルファベットからなる配列
+alp=[chr(ord('a')+i) for i in range(26)] # 小文字のアルファベットからなる配列
