b96dab6b1178389e9827d4946352c2f0d92a4f14
Tohru-246ra <108559601+Tohru-246ra@users.noreply.github.com>
2023-02-06 09:34:30 +0900

    Add files via upload

---
+++ others/characterCode.py
@@ -0,0 +1,6 @@
+alp=[chr(ord('A')+i) for i in range(26)]
+# print(ord('あ')) #ord 文字→コードポイント(10進数,Unicode)
+# print(hex(ord('あ')))
+# print(chr(0x3042)) #chr: コードポイント→文字
+hiragana=[chr(ord('あ')+i) for i in range(50)]
+print(hiragana)
